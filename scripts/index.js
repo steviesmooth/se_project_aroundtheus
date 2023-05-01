cdf5ca09b9a9cc74a280610c1c41f7e295342d1c
steviesmooth <naraksteve@yahoo.com>
2023-05-01 16:18:57 -0500

    stage 3 java edit btn

--- scripts/index.js
+++ scripts/index.js
@@ -24,4 +24,16 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
-console.log(initialCards);
+
+const profileEditButton = document.querySelector("#profile-edit-button");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+
+profileEditButton.addEventListener("click", () => {
+  profileEditModal.classList.add("modal__opened");
+});
+
+const modalClosedButton = document.querySelector("#modal-close-button");
+
+modalClosedButton.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal__opened");
+});
